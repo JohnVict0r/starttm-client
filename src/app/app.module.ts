59cc7286f68976d195394d26347537c18c8e8d6c
johnalves <johnvictorio3@gmail.com>
2019-01-14 12:29:58 -0300

    Redux store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,10 @@ import { SegurancaModule } from './seguranca/seguranca.module';
 import { CoreModule } from './core/core.module';
 import { JwtInterceptor } from './_helpers/jwt.interceptor';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environemnt
+
 @NgModule({
   imports: [
     BrowserAnimationsModule,
@@ -31,6 +35,11 @@ import { JwtInterceptor } from './_helpers/jwt.interceptor';
     CoreModule,
     SegurancaModule,
     AppRoutingModule,
+    //StoreModule.forRoot(reducers), // Instrumentation must be imported after importing StoreModule (config is optional)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   declarations: [
     AppComponent,
